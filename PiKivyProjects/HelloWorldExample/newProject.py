46c9c674e182eb5ee232ca1c71b8754b8797ca89
Hannah Kleidermacher <hannah.kleidermacher@gmail.com>
2018-02-15 12:13:32 -0800

    reverted newProject.py to Will's version

--- PiKivyProjects/HelloWorldExample/newProject.py
+++ PiKivyProjects/HelloWorldExample/newProject.py
@@ -1,119 +1,49 @@
 import os, shutil
 
 MAIN_FILE_TEMPLATE = """
-# ////////////////////////////////////////////////////////////////
-# //                     IMPORT STATEMENTS                      //
-# ////////////////////////////////////////////////////////////////
-
-
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import *
 
-
-# ////////////////////////////////////////////////////////////////
-# //            DECLARE APP CLASS AND SCREENMANAGER             //
-# //                     LOAD KIVY FILE                         //
-# ////////////////////////////////////////////////////////////////
-
+Builder.load_file('main.kv')
 
 sm = ScreenManager()
+Window.clearcolor = (.95,.95,.95, 1) # Background Color of white
+
+# ALL HARDWARE SETUP SHOULD APPEAR BETWEEN THESE LINES
+
+
+# ^^^
+
+
+class MainScene(Screen):  # creates a scene called MainScene
+    def buttonAction(self):
+        pass
 
 class MyApp(App):
     def build(self):
         return sm
 
-Builder.load_file('kivy.kv')
-Window.clearcolor = (0.1, 0.1, 0.1, 1) # (WHITE)
-
-
-# ////////////////////////////////////////////////////////////////
-# //                    SLUSH/HARDWARE SETUP                    //
-# ////////////////////////////////////////////////////////////////
-
-
-
-
-# ////////////////////////////////////////////////////////////////
-# //                      GLOBAL VARIABLES                      //
-# //                         CONSTANTS                          //
-# ////////////////////////////////////////////////////////////////
-
-
-
-
-# ////////////////////////////////////////////////////////////////
-# //                       MAIN FUNCTIONS                       //
-# //             SHOULD INTERACT DIRECTLY WITH HARDWARE         //
-# ////////////////////////////////////////////////////////////////
-
-
-def quitAll():
-    # free stepper motors, etc. before quit()
-    quit() # quits program
-#  more functions here
-
-
-# ////////////////////////////////////////////////////////////////
-# //        DEFINE MAINSCREEN CLASS THAT KIVY RECOGNIZES        //
-# //                                                            //
-# //   KIVY UI CAN INTERACT DIRECTLY W/ THE FUNCTIONS DEFINED   //
-# //     CORRESPONDS TO BUTTON/SLIDER/WIDGET "on_release"       //
-# //                                                            //
-# //   SHOULD REFERENCE MAIN FUNCTIONS WITHIN THESE FUNCTIONS   //
-# //      SHOULD NOT INTERACT DIRECTLY WITH THE HARDWARE        //
-# ////////////////////////////////////////////////////////////////
-
-
-class MainScreen(Screen):
-
-    def exitProgram(self):
-        quitAll()
-    # more functions here
-	
-sm.add_widget(MainScreen(name = 'main'))
-
-
-# ////////////////////////////////////////////////////////////////
-# //                          RUN APP                           //
-# ////////////////////////////////////////////////////////////////
-
-
-MyApp().run()
-
+sm.add_widget(MainScene(name='main'))
+MyApp().run()  # starts running app
 """
 
 KV_FILE_TEMPLATE = """
-<MainScreen>:
+<MainScene>:    #start scene with a button and label
     name: 'main'
     FloatLayout:
-        size_hint: None, None
-
-		# QUIT BUTTON
+        size_hint: None, None  #Needed to be able to define define height and width of widgets
         Button:
-            text: 'X'
-            font_size: 23
             size_hint: None,None
-            size: root.width * 0.015,root.width * 0.015
-            x: root.width * 0.965
-            y: root.height * 0.945
-            background_color: 0.7, 0.7, 0.7, 1
-            on_release: root.exitProgram()
-
-		# IMAGE
-        Screen:
-            Image:
-                source: 'DPEA Logo Transparent.png'
-                keep_ratio: False
-                allow_stretch: True
-                opacity: 1
-                size_hint: 2.7, 2.7
-                x: root.width * 0.753
-                y: root.height*0.283
+            text: 'text'
+            font_size: 20
+            size: 500, 75
+            background_color: .1,.1,1,1
+            center: root.center
+            on_release: root.buttonAction()
 """
 
 
