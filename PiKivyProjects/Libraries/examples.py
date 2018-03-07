7d404b6a09af27976bc326bd389d6c03fb479841
Francis Pan <francisypan@gmail.com>
2018-03-06 17:50:21 -0800

    added pause scene libraries

--- PiKivyProjects/Libraries/examples.py
+++ PiKivyProjects/Libraries/examples.py
@@ -4,7 +4,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.image import Image
@@ -12,7 +12,10 @@ from kivy.uix.widget import Widget
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import *
+from kivy.clock import Clock
+from kivy.animation import Animation
 
+import time
 import ImageButton
 
 sm = ScreenManager()
@@ -23,7 +26,19 @@ class MyApp(App):
 
 Builder.load_file('examples.kv')
 Builder.load_file('DPEAButton.kv')
+Builder.load_file('PauseScene.kv')
+
 Window.clearcolor = (0.95, 0.95, 0.95, 1)
+
+def pause(text, sec):
+    sm.current = 'pauseScene'
+    sm.current_screen.ids.pauseText.text = text
+    Clock.schedule_once(transitionBack, sec)
+    load = Animation(size = (150, 15), duration = sec) + Animation(size = (15, 15), duration = 0)
+    load.start(sm.current_screen.ids.progressBar)
+
+def transitionBack(dt):
+    sm.current = 'examples'
 
 class MainScreen(Screen):
     def DPEAButtonDown(self):
@@ -36,6 +51,13 @@ class MainScreen(Screen):
         self.ids.DPEAButton.color = 0.019, 0.337, 1, 1
         self.ids.X.color = 1, 0, 0, 1
 
+    def DPEAButtonUp(self):
+        pause('Scene Paused', 5)
+
+class PauseScene(Screen):
+    pass
+
 sm.add_widget(MainScreen(name = 'examples'))
+sm.add_widget(PauseScene(name = 'pauseScene'))
 
 MyApp().run()
