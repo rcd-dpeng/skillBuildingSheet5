4c967f91ee894975c31133dbfdcc18b407dcc91f
Hannah Kleidermacher <hannah.kleidermacher@gmail.com>
2018-05-07 08:42:10 -0700

    created RPiMIB library

---
+++ RPiMIB/RPiMIB.py.py
@@ -0,0 +1,56 @@
+#! usr/bin/env python3
+import spidev
+import os
+from time import sleep
+##import RPi.GPIO as GPIO
+
+##GPIO.setmode(GPIO.BOARD)
+##GPIO.setup(40, GPIO.IN)    
+##GPIO.setup(15, GPIO.IN)
+
+# set up SPI between the Raspberry Pi and the PiMIB board
+spi = spidev.SpiDev()
+spiFrequency = 1000000
+
+
+
+#takes 0, 1, or 2
+def readEncoder(encoder):
+    spi.open(0,0)
+    spi.xfer([0x00, 0x10], spiFrequency, 1)                      # sending this command tells PiMIB to send back encoder data
+    enc0_list_of_bytes = spi.xfer([0x00, 0x00], spiFrequency, 1)  # getting data for encoder A
+    enc1_list_of_bytes = spi.xfer([0x00, 0x00], spiFrequency, 1)
+    enc2_list_of_bytes = spi.xfer([0x00, 0x00], spiFrequency, 1)
+    enc0 = 0x100*enc0_list_of_bytes[0] + enc0_list_of_bytes[1]  # convert the two bytes received into encoder value
+    enc1 = 0x100*enc1_list_of_bytes[0] + enc1_list_of_bytes[1]
+    enc2 = 0x100*enc2_list_of_bytes[0] + enc2_list_of_bytes[1]
+
+    if encoder == 0:
+        if (enc0 & 0xf000) :         # encoder has 12-bit value  PiMIB sends back xF000 if encoder not pulgged in
+            print("Encoder A not plugged in")
+        else :
+            return enca
+    if encoder == 1:
+        if (enc1 & 0xf000) :
+            print("Encoder B not plugged in")
+        else :
+            return encb
+    if encoder == 2:
+        if (enc2 & 0xf000) :
+            print("Encoder C not plugged in")
+        else :
+            return encc
+
+    spi.close()
+
+def sendSPI(address, data1, data2):
+    spi.open(0,0)
+    spi.xfer([0x00, address], spiFrequency, 1)  # command to write the next data to Function Generator 1
+    spi.xfer([data1, data2], spiFrequency, 1)  #  data 0x1234 sent to function generator 1 using the SPI connector 1
+    spi.close()
+
+def sendI2C(address, data):
+    spi.open(0,0)
+    spi.xfer([0x00, 0x80], spiFrequency, 1)  #command to write following data over I2C on connector 1
+    spi.xfer([address, data], spiFrequency, 1)  # I2C write to IC address 0x41 with data 0x17  0x41 = Amp1
+    spi.close()
