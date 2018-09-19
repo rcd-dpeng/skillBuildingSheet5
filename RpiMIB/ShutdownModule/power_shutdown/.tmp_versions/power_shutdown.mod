ce5ab3231344783ccd5eca9536b7468c0c887745
mileskretschmer <mileskretschmer@dpengineering.org>
2018-09-19 14:04:00 -0700

    Adding the power shutdown kernel module.

---
+++ RpiMIB/ShutdownModule/power_shutdown/.tmp_versions/power_shutdown.mod
@@ -0,0 +1,3 @@
+/home/pi/Documents/Kernel_Modules/power_shutdown/power_shutdown.ko
+/home/pi/Documents/Kernel_Modules/power_shutdown/power_shutdown.o
+
---
+++ RpiMIB/ShutdownModule/power_shutdown/power_shutdown.mod.c
@@ -0,0 +1,44 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x13e75575, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x3c35e63b, __VMLINUX_SYMBOL_STR(gpiod_unexport) },
+	{ 0xc1514a3b, __VMLINUX_SYMBOL_STR(free_irq) },
+	{ 0xd6b8e852, __VMLINUX_SYMBOL_STR(request_threaded_irq) },
+	{ 0xb6c1e257, __VMLINUX_SYMBOL_STR(gpiod_to_irq) },
+	{ 0x211c4d53, __VMLINUX_SYMBOL_STR(gpiod_direction_input) },
+	{ 0xb14e8416, __VMLINUX_SYMBOL_STR(gpiod_export) },
+	{ 0xbaeef7b0, __VMLINUX_SYMBOL_STR(gpio_to_desc) },
+	{ 0x2e5810c6, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr1) },
+	{ 0xa7eedcc4, __VMLINUX_SYMBOL_STR(call_usermodehelper) },
+	{ 0xb1ad28e0, __VMLINUX_SYMBOL_STR(__gnu_mcount_nc) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "230E78FA8073C31B221A671");
