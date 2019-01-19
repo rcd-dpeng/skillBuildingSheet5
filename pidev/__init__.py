4508c742b45fa463369b559a3f6e51c53dd356b7
Nathan Wachholz <nmwachholz+github@gmail.com>
2019-01-19 15:18:45 -0800

    fix imports

--- pidev/__init__.py
+++ pidev/__init__.py
@@ -1,6 +1,6 @@
-# from .RPiMIB import *
-# from .speaker import *
-# from .stepper import *
-# from .slush_manager import slush_board
-# from .SlaveMaster import Callable, Arduino, SerialMaster
+from .RPiMIB import *
+from .speaker import *
+from .stepper import *
+from .slush_manager import slush_board
+from .SlaveMaster import Callable, Arduino, SerialMaster
 from .MixPanel import *
