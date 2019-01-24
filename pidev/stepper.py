7f89438f19f3c6947daa78ffef066910c1f52fe5
Wade Varesio <wadevaresio@gmail.com>
2019-01-24 13:46:53 -0800

    Added function to read registers from the motor controller chip

--- pidev/stepper.py
+++ pidev/stepper.py
@@ -4,9 +4,7 @@ from Slush.Devices import L6470Registers as LReg
 from .slush_manager import slush_board as b
 b = Slush.sBoard()
 
-
 class stepper(Slush.Motor):
-
     '''
     The init function sets a range of parameters:
     port, micro_steps, run_current, accel_current, deaccel_current
@@ -19,6 +17,7 @@ class stepper(Slush.Motor):
 
     def __init__(self, **kwargs):
         super().__init__(kwargs.get("port", 0))
+        self.port = kwargs.get("port", 0)
         self.resetDev()
         self.micro_steps = kwargs.get("micro_steps", 64)
         self.set_micro_steps(self.micro_steps)
@@ -30,6 +29,26 @@ class stepper(Slush.Motor):
         self.speed = kwargs.get("speed", 1)
         self.set_speed(self.speed)
         self.getParam(LReg.CONFIG) == 0x2e88  # Allow for GPIO on the slushengine
+
+    def print_status(self):
+        """
+        Print all of the registers status for the L6470 chipset
+        :return:
+        """
+        chip_statuses = ['HiZ', 'Busy', 'SW_F', 'SW_EVEN', 'DIR', 'MOT_STATUS', 'NOTPERF_CMD',
+                    'WRONG_CMD', 'UVLO', 'TH_WRN', 'TH_SD', 'OCD', 'STEP_LOSS_A', 'STEP_LOSS_B', 'SCK_MOD']
+
+        byte = '{0:b}'.format(self.getStatus())
+        print("The byte for ", str(self), "is: ", byte)
+
+        for index in range(0, len(byte)):
+            if index == 5:
+                print("MOT_STATUS: " + str(int(byte[index])) + str(int(byte[index])))
+                index+=1
+            if index == 6:
+                continue
+            else:
+                print(chip_statuses[index] + ": " + str(int(byte[index])))
 
     '''
     these are the functions used to change parameters
@@ -158,6 +177,12 @@ class stepper(Slush.Motor):
     def set_max_speed(self, speed):
         self.setMaxSpeed(speed)
 
+    def wait_to_return(self):
+        while self.isBusy():
+            continue
+
+    def __repr__(self):
+        return "stepper on port"  + str(self.port)
     '''
     Some other helpful functions from motor that may be used
     
