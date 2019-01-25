cd6ffc744ad4115dad2867c6abcafb8aef789f8b
Wade Varesio <wadevaresio@gmail.com>
2019-01-25 11:49:32 -0800

    print_status() now prints from the MSB to LSB correctly

--- pidev/stepper.py
+++ pidev/stepper.py
@@ -1,21 +1,22 @@
 import sys
+from collections import OrderedDict
 import Slush
 from Slush.Devices import L6470Registers as LReg
 from .slush_manager import slush_board as b
+
 b = Slush.sBoard()
 
-class stepper(Slush.Motor):
-    '''
-    The init function sets a range of parameters:
-    port, micro_steps, run_current, accel_current, deaccel_current
-    hold_current, steps_per_unit, speed
 
-    There are 2 ways to change them:
-    1) in the constructor ex) arm = stepper (port = 0, run_current = 5)
-    2) change it within main code by calling set functions ex) arm.set_speed(100)
-    '''
+class stepper(Slush.Motor):
 
     def __init__(self, **kwargs):
+        """
+        Constructor for a stepper motor on the Slush Engine
+        he init function sets a range of parameters:
+        port, micro_steps, run_current, accel_current, deaccel_current
+        hold_current, steps_per_unit, speed
+        :param kwargs: Reference note on constructor summary
+        """
         super().__init__(kwargs.get("port", 0))
         self.port = kwargs.get("port", 0)
         self.resetDev()
@@ -32,31 +33,56 @@ class stepper(Slush.Motor):
 
     def print_status(self):
         """
-        Print all of the registers status for the L6470 chipset
-        :return:
+        Print all of the registers status for the L6470 chipset, if a status has a \ means it is active low
+        :return: None
         """
-        chip_statuses = ['HiZ', 'Busy', 'SW_F', 'SW_EVEN', 'DIR', 'MOT_STATUS', 'NOTPERF_CMD',
-                    'WRONG_CMD', 'UVLO', 'TH_WRN', 'TH_SD', 'OCD', 'STEP_LOSS_A', 'STEP_LOSS_B', 'SCK_MOD']
+        chip_statuses = OrderedDict([
+            ('SCK_MOD', 1),
+            ('STEP_LOSS_B\\', 1),
+            ('STEP_LOSS_A\\', 1),
+            ('OCD\\', 1),
+            ('TH_SD\\', 1),
+            ('TH_WRN\\', 1),
+            ('UVLO\\', 1),
+            ('WRONG_CMD', 1),
+            ('NOTPERF_CMD', 1),
+            ('MOT_STATUS', 2),
+            ('DIR', 1),
+            ('SW_EVEN', 1),
+            ('SW_F', 1),
+            ('BUSY\\', 1),
+            ('HiZ', 1)])
 
         byte = '{0:b}'.format(self.getStatus())
+
+        if len(byte) != 16:  # Add leading zeroes to the byte to make it 16 bits
+            diff = 16 - len(byte)
+            byte = "0" * diff + byte
+
         print("The byte for ", str(self), "is: ", byte)
 
-        for index in range(0, len(byte)):
-            if index == 5:
-                print("MOT_STATUS: " + str(int(byte[index])) + str(int(byte[index])))
-                index+=1
-            if index == 6:
-                continue
-            else:
-                print(chip_statuses[index] + ": " + str(int(byte[index])))
+        location = 0
 
-    '''
-    these are the functions used to change parameters
-    '''
+        for status in chip_statuses:
+            data = ""
+            for i in range(0, chip_statuses[status]):
+                data += str(int(byte[location]))
+                location += 1
+            print(status + ": " + data)
+
     def get_micro_steps(self):
+        """
+        Get the number of microsteps the stepper motor is running at
+        :return:
+        """
         return self.micro_steps
 
     def set_micro_steps(self, micro_steps):
+        """
+        Set the number of microsteps the stepper motor runs at
+        :param micro_steps: Number of microsteps of base 2 up to a maximum of 128
+        :return: None
+        """
         if micro_steps > 128:
             sys.exit("ERROR: Slush Engine only supports microstepping values of base 2 up to a maximum of 128")
 
@@ -64,20 +90,36 @@ class stepper(Slush.Motor):
         self.setMicroSteps(micro_steps)
 
     def set_speed(self, speed):
+        """
+        Set the speed the stepper motor runs at
+        :param speed: Speed the motor will run at
+        :return: None
+        """
         self.speed = speed * self.steps_per_unit
         self.set_max_speed(self.speed)
 
     def set_accel(self, acceleration):
+        """
+        Set the acceleration of the motor
+        :param acceleration: Acceleration amount
+        :return: None
+        """
         self.setAccel(acceleration)
 
     def set_deaccel(self, deceleration):
+        """
+        Set the deceleration for the motor
+        :param deceleration: Deceleration amount
+        :return: None
+        """
         self.setDecel(deceleration)
 
-    '''
-    -moves until the switch is triggered
-    -dependent on a direction: 0 or 1
-    '''
     def home(self, direction):
+        """
+        Home the motor
+        :param direction: Direction (0 or 1) the motor should spin when homing 0-clockwise
+        :return: None
+        """
         self.run(direction, self.speed)
 
         while not self.read_switch():
@@ -86,64 +128,86 @@ class stepper(Slush.Motor):
         self.hard_stop()
         self.set_as_home()
 
-    '''
-    reads the switch in the respective port
-    '''
-
     def read_switch(self):
-        if self.getStatus() & 0x4: return 1
-        else: return 0
+        """
+        Read the stepper motors corresponding switch
+        :return: None
+        """
+        if self.getStatus() & 0x4:
+            return 1
+        else:
+            return 0
 
-    '''
-    moves a certain distance with movements blocked (synchronise)
-    '''
     def relative_move(self, distance):
+        """
+        Moves a certain distance with MOVEMENTS BLOCKED (synchronise)
+        :param distance:
+        :return:
+        """
         number_of_steps = distance * self.micro_steps * self.steps_per_unit
         self.move(int(number_of_steps))
         self.wait_move_finish()
 
-    '''
-    starts moving a certain distance without blocking movements (a-synchronise)
-    '''
     def start_relative_move(self, distance):
+        """
+        Starts moving a certain distance WITHOUT BLOCKING MOVEMENTS (a-synchronise)
+        :param distance:
+        :return:
+        """
         number_of_steps = distance * self.micro_steps * self.steps_per_unit
         self.move(int(number_of_steps))
 
-    '''
-    Goes to a set position with blocking (synchronise)
-    '''
     def go_to_position(self, distance):
+        """
+        Goes to a set position WITH BLOCKING (synchronise)
+        :param distance: Distance to move to
+        :return: None
+        """
         number_of_steps = distance * self.micro_steps * self.steps_per_unit
         self.go_to(int(number_of_steps))
         self.wait_move_finish()
 
-
-    '''
-    begins going to a set position without blocking (a-synchronise)
-    '''
     def start_go_to_position(self, distance):
+        """
+        begins going to a set position WITHOUT BLOCKING (a-synchronise)
+        :param distance: distance to move to
+        :return: None
+        """
         number_of_steps = distance * self.micro_steps * self.steps_per_unit
         self.go_to(int(number_of_steps))
 
-
-    '''
-    returns the state of a sensor/switch  
-    port:   a = 0; b = 1
-    pin: first pin = 0, goes up by 1 per pin
-    '''
     @staticmethod
     def get_GPIO_state(port, pin):
+        """
+        Get the state of one of the GPIO pins
+        :param port: GPIO port a=0, b=1
+        :param pin: Pin to check, first pin=0 incrementing by one
+        :return:
+        """
         return b.getIOState(port, pin)
 
     @staticmethod
     def set_GPIO_state(port, pin, state):
+        """
+        Set the state of one of the GPIO pins
+        :param port: GPIO port a=0, b=1
+        :param pin: Pin to set, first pin=0 incrementing by one
+        :param state: State you want to set the pin at
+        :return: None
+        """
         return b.setIOState(port, pin, state)
 
     '''
     If True motor with stop when sensor is high (Should be used in cases where there is a mechanical stop)
     If False motor will continue past sensor (Should be used when motor can rotate freely)
     '''
+
     def set_limit_hardstop(self, stop):
+        """
+        Set whether the Slush Engine should stop moving
+        :param stop:
+        :return:
+        """
         try:
             self.setLimitHardStop(stop)
         except AttributeError:
@@ -152,6 +216,7 @@ class stepper(Slush.Motor):
     '''
     hard stops the motor
     '''
+
     def stop(self):
         self.hard_stop()
 
@@ -162,6 +227,7 @@ class stepper(Slush.Motor):
     
     stepper extends motors so must adjust the functions accordingly
     '''
+
     def hard_stop(self):
         self.hardStop()
 
@@ -182,7 +248,8 @@ class stepper(Slush.Motor):
             continue
 
     def __repr__(self):
-        return "stepper on port"  + str(self.port)
+        return "stepper on port" + str(self.port)
+
     '''
     Some other helpful functions from motor that may be used
     
