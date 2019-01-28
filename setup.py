a632d70a683c6d2da8269a3e73f3ce8e4b49c4df
Wade Varesio <wadevaresio@gmail.com>
2019-01-27 18:37:18 -0800

    Add ODrive to pidev

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name="pidev",
     version='0.1dev',
-    packages=["pidev", "pidev.kivy"],
+    packages=["pidev", "pidev.kivy", "pidev.odrive"],
     long_description="A DPEA package containing RPi infrastructure helpers.",
     install_requires=[
-        "pyserial", "requests"
+        "pyserial", "requests", "odrive"
     ]
 )
