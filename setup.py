8a85795ffeb0a923049321c4b8a8a0dcde8202ac
Wade Varesio <wadevaresio@gmail.com>
2019-02-07 13:46:23 -0800

    Remove odrive from pidev

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name="pidev",
     version='0.1dev',
-    packages=["pidev", "pidev.kivy", "pidev.odrive"],
+    packages=["pidev", "pidev.kivy"],
     long_description="A DPEA package containing RPi infrastructure helpers.",
     install_requires=[
-        "pyserial", "requests", "odrive"
+        "pyserial", "requests"
     ]
 )
