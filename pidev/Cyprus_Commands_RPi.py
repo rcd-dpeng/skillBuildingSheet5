a6fa994b859251db9c96b16156a9e424d52a7f0e
Unknown <mileskretschmer@dpengineering.org>
2019-02-12 08:18:56 -0800

    Fixed latent errors in commands

--- pidev/Cyprus_Commands_RPi.py
+++ pidev/Cyprus_Commands_RPi.py
@@ -23,18 +23,19 @@ def spi_read_word(): #reads the spi value sent to the RPi as a 16 bit word
 
 def open_spi(): #opens spi communication
     spi.open(0,0)
+    spi.mode = 0b00
 
 def close_spi(): #closes spi communication
     spi.close()
 
 def read_spi(port, channel): #returns the response recieved by the cyprus from the given port and channel
-    command_data = 0x0300 + (port << 4) + channel
+    command_data = 0x0300 | (port << 4) | channel
     spi_write_word(command_data)
     sleep(2*delay)
     return spi_read_word()
 
 def write_spi(port, channel, value): #writes the given value to the given spi port and channel of the cyprus
-    command_data = 0x0400 + (port << 4) + channel
+    command_data = 0x0400 | (port << 4) | channel
     spi_write_word(command_data)
     sleep(delay)
     spi_write_word(value)
@@ -42,26 +43,26 @@ def write_spi(port, channel, value): #writes the given value to the given spi po
 def write_pwm(port, parameter, value): #changes the given paremeter, either "compare mode", "period", or "compare",
     if (parameter == "compare mode"):  #of the given port to the given value. Compare modes: 0:<, 1:<=, 2:>, 3:>=, 4:=
         parameter_number = 0
-	processed_value = value
+        processed_value = value
     elif (parameter == "period"):
         parameter_number = 1
-	processed_value = int(value * pwm_clock_frequency)
+        processed_value = int(value * pwm_clock_frequency)
     elif (parameter == "compare"):
         parameter_number = 2
-	processed_value = int(value * pwm_clock_frequency)
+        processed_value = int(value * pwm_clock_frequency)
     else:
         return "parameter not recognized"
-    command_data = 0x0500 + (port << 4) + parameter_number
+    command_data = 0x0500 | (port << 4) | parameter_number
     spi_write_word(command_data)
     sleep(delay)
     spi_write_word(processed_value)
-
+	
 def setup_servo(port): #sets up the given pwm port to control a servo
 	write_pwm(port, "compare mode", 1)
 	write_pwm(port, "period", .02)
 	
 def write_servo_position(port, position): #sets servo on given port to position given by a number in the interval
-	if (position > 1): 		  # [0, 1], where 0 corresponds to one end of its range and 1 to the other
+	if (position > 1): 		              # [0, 1], where 0 corresponds to one end of its range and 1 to the other
 		position = 1
 	elif (position < 0):
 		position = 0
@@ -117,8 +118,8 @@ def set_encoder_trigger(channel, value): #sets trigger on given channel to given
     sleep(delay)
     spi_write_word(value)
     
-def read_encoder(channel): #returns the value from the encoder at the given channel
-    command_data = 0x0b00 | channel
+def read_encoder(port, channel): #returns the value from the encoder at the given channel
+    command_data = 0x0b00 | (port << 4) | channel
     spi_write_word(command_data)
     sleep(2*delay)
     return spi_read_word()
