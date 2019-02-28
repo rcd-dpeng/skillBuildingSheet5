124165f48372c1343124317185cbce69a738316d
Wade Varesio <wadevaresio@gmail.com>
2019-02-28 10:57:13 -0800

    Potential fix

--- pidev/kivy/DPEAButton.py
+++ pidev/kivy/DPEAButton.py
@@ -1,13 +1,18 @@
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 import os.path
 
 dpea_button_kv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "", "DPEAButton.kv")
 Builder.load_file(dpea_button_kv_path)
+shadow_image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "shadow.png")
+
+shadow_image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "shadow.png")
+
+# def __init__(self):
+#     super.__init__()
 
 
 class DPEAButton(Button):
     shadow_image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "shadow.png")
-
-    def __init__(self):
-        super.__init__()
+    shadow_path = ObjectProperty(shadow_image_path)
